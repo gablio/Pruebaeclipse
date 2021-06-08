eaffb74d61224036db97ff12d5dcee824a74da08
Julio Gabriel Milano <gablio@live.com>
2021-06-08 05:00:01 -0300

    variables agregadas

--- src/ejemplogit.java
+++ src/ejemplogit.java
@@ -4,6 +4,10 @@ public class ejemplogit {
 	public static void main(String[] args) {
 		// TODO Apéndice de ;método generado automáticamente
 System.out.println("julio");
+
+int edad;
+edad = 50;
+System.out.println(edad);
 	}
 
 }
